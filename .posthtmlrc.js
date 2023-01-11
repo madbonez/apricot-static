6e25dc60a805d5bcba522b61975acdf4fb6b3e6b
SashaMalova <zapa-m@yandex.ru>
2023-01-11 12:38:26 +0300

    test

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -7,20 +7,27 @@ const plugins = {
     "posthtml-include": {},
     "posthtml-expressions": {
         "locals": {
-            "slides": [
-                {
+            "text": "111",
+            "title": "222",
+            "block_3" : [{
+                "title": "About us",
+                "text": "Apricot is a startup in the field of Immersive Technologies",
+                "slides": [{
                     "text": "We develop solutions for heavy and light industries, armed forces, as well as civil engineering solutions, based on the technologies of virtual and augmented reality",
                     "image": "././public/image/screen3/01.png"
-                },
-                {
+                },{
                     "text": "Apart from the design of solutions \r\nand technical support, we also provide our customers services in consulting and strategic planning in the field  of emerging technologies.",
                     "image": "././public/image/screen3/02.png"
-                },
-                {
+                },{
                     "text": "We are a team of highly qualified experts \r\nin immersive technologies with vast experience and an impressive portfolio of successfully implemented projects in various industries, including defense, education, and entertainment.",
                     "image": "././public/image/screen3/03.png"
-                }
-            ]
+                }]
+            }],
+            "block_4" : [{
+                "title": "Our Goals",
+                "text": "Creation of breakthrough technologies in the field of training simulators and Industry 4.0"
+            }]
+
         }
     }
 }
